af74a8d50210bd9e05527ce4c5b643ad89d0cd4d
Ragaey <mragaey@arkdev.net>
2018-05-17 12:34:04 +0200

    adding sourcemap and enable hot replacement modules

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,9 +2,10 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common')
 module.exports = merge(common, {
   mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
-    host: 'localhost',
-    port: 3000,
+    contentBase: './dist',
+    hot: true,
     open: true
   }
 })
